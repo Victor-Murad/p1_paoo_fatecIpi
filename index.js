36e72d29eb96517af2b6b4877b691d631c4c0625
Victor Kenji <victorkmurad@gmail.com>
2025-01-28 18:23:02 -0300

    feat(p1): obtem previsao a partir de coords

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 require("dotenv").config();
 const {
-    APPID, Q, LIMIT, URLBASE,
+    APPID, Q, LIMIT, URLBASE, URLBASE2, UNIT
 } = process.env
 
 const temp =()=>{
@@ -12,6 +12,13 @@ const temp =()=>{
         const latitude = res.data[0].lat
         const longitude = res.data[0].lon
         console.log(`${nome}:latitude:${latitude},longitude:${longitude}`)
+    const url2 = `${URLBASE2}?lat=${latitude}&lon=${longitude}&units=${UNIT}&appid=${APPID}`
+    axios.get(url2)
+    .then(res=>{
+        const feels_like = res.data.main.feels_like
+        const description = res.data.weather[0].description
+        console.log(`sensação térmica:${feels_like},\ndescription:${description}`)
+    })
     })
 }
 temp()
